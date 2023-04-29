1f3caea30c18be0f35f5d78f0d5dda5759144497
Liza337 <jucse28.337@gmail.com>
2023-04-29 12:52:30 +0600

    form added

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -2,8 +2,23 @@ import React from 'react';
 import './SignUp.css';
 const SignUp = () => {
     return (
-        <div>
-            <h2>please sign up first</h2>
+        <div className='form-container'>
+            <h2 className='form-title'>Register your account</h2>
+            <form>
+                 <div className="form-control">
+                     <label htmlFor="email">Email</label>
+                        <input type="email" name="email" id="" required/>
+                 </div>
+                 <div className="form-control">
+                     <label htmlFor="password">Password</label>
+                     <input type="password" name="password" id="" required/>
+                 </div>
+                 <div className="form-control">
+                     <label htmlFor="password">Confirm Password</label>
+                     <input type="password" name="confirm" id="" required/>
+                 </div>
+                 <input className='btn-submit' type="submit" value="Register" />
+            </form>
         </div>
     );
 };
